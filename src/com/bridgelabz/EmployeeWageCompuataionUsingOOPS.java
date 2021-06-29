9a67243ec7a061726fda95cfb763685b0b7c3d7d
mayavathi <mayachandre27@gmail.com>
2021-06-29 04:17:44 -0400

    total wage for each company

--- src/com/bridgelabz/EmployeeWageCompuataionUsingOOPS.java
+++ src/com/bridgelabz/EmployeeWageCompuataionUsingOOPS.java
@@ -19,17 +19,10 @@ public class EmployeeWageCompuataionUsingOOPS
 	        this.wage_per_hr = wage_per_hr;
 	    }
 	    public static void main(String[] args) {
-	        Scanner s = new Scanner(System.in);
-	        System.out.print("Enter a company name : ");
-	        String companyName = s.next();
-	        System.out.print("Enter maximum work hours of month : ");
-	        int max_working_hrs = s.nextInt();
-	        System.out.print("Enter maximum days in month : ");
-	        int days_in_month = s.nextInt();
-	        System.out.print("Enter wage per hour : ");
-	        int wage_per_hr = s.nextInt();
-	        EmployeeWage e = new EmployeeWage(companyName, max_working_hrs, days_in_month, wage_per_hr);
-	        e.employeeWage();
+	    	EmployeeWage e1 = new EmployeeWage("d-mart", 100, 20, 100);
+	        EmployeeWage e2 = new EmployeeWage("jio-Mart", 150, 25, 110);
+	        e1.employeeWage();
+	        e2.employeeWage();
 
 	    }
 
@@ -39,7 +32,8 @@ public class EmployeeWageCompuataionUsingOOPS
 	        int total_working_hr = 0;
 	        int monthly_total_wage = 0;
 	        int daily_wage_array[] = new int[days_in_month];
-	        for (int i = 0; i < days_in_month && total_working_hr < max_working_hrs; i++) {
+	        int days = 0;
+	        while (days < days_in_month && total_working_hr < max_working_hrs) {
 	            int emp_check = (int) (Math.floor(Math.random() * 10)) % 3;
 	            switch (emp_check) {
 	                case IS_FUllTime -> {
@@ -53,14 +47,15 @@ public class EmployeeWageCompuataionUsingOOPS
 	                }
 	            }
 	            total_working_hr += working_hr;
-	            daily_wage_array[i] = working_hr * wage_per_hr;
+	            daily_wage_array[days] = working_hr * wage_per_hr;
+	            days++;
 	        }
 	        for (int j = 0; j < days_in_month; j++) {
 	            int day = j + 1;
 	            System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
 	            monthly_total_wage = monthly_total_wage + daily_wage_array[j];
 	        }
-	        System.out.println("\nMonthly wage is " + monthly_total_wage);
+	        System.out.println("\nMonthly wage for " +companyName+ ";" + monthly_total_wage);
 	    }
 
 }
